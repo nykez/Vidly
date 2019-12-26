9bb62d4711cde5f54914c64319ca26745e2cb899
Jordan C <nykeztv@users.noreply.github.com>
2019-12-26 02:06:34 -0500

    add all previous validation from customer to movie classes

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,6 +9,8 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
 
         public Genre Genre { get; set; }
@@ -18,11 +20,14 @@ namespace Vidly.Models
         public byte GenreId { get; set; }
 
         [Display(Name = "Release Date")]
+        [Required]
         public DateTime ReleaseDate { get; set; }
 
         public DateTime DateAdded { get; set; }
 
+        [Required]
         [Display(Name = "Number in Stock")]
+        [Range(1, 20)]
         public byte NumberInStock { get; set; }
     }
 }
