cfec5ab97fc04ca916a74520b70bfb554c05102b
Jordan C <nykeztv@users.noreply.github.com>
2019-12-26 01:42:07 -0500

    some more clientide validation

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -105,6 +105,7 @@ namespace Vidly.Controllers
 
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes,
             };
             
